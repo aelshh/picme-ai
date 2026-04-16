28793deb09205575a2bbc93ada95fb9028e0c82a
Adarsh Chaudhary <chaudharyadarsh9919@gmail.com>
2026-04-16 18:32:13 +0530

    Migrate to cloudflare workers AI

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,9 @@ const nextConfig: NextConfig = {
         hostname: "oyobadaucviihcmxpgan.supabase.co"
       }
     ],
+    domains: [
+      "pub-0f6d8906e6134f709ae4318a409f1309.r2.dev"
+    ]
   },
 };
 
